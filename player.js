d86aeef45315293d6b13f06786db8a4e61b2d517
Kito-Anime <antayteam@gmail.com>
2026-03-14 23:44:30 +0700

    Update player.js

--- player.js
+++ player.js
@@ -5,28 +5,37 @@ doc,
 getDoc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const params=new URLSearchParams(location.search);
-const id=params.get("id");
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
 
-const ref=doc(db,"anime",id);
-const snap=await getDoc(ref);
+async function loadAnime(){
 
-const data=snap.data();
+const ref = doc(db,"anime",id);
+const snap = await getDoc(ref);
 
-document.getElementById("title").textContent=data.title;
+if(!snap.exists()){
+return;
+}
 
-const video=document.getElementById("video");
+const data = snap.data();
+
+document.getElementById("title").textContent = data.title;
+
+const video = document.getElementById("video");
 
 if(Hls.isSupported()){
 
-const hls=new Hls();
+const hls = new Hls();
 
 hls.loadSource(data.video);
 hls.attachMedia(video);
 
-}
-else{
+}else{
 
-video.src=data.video;
+video.src = data.video;
 
 }
+
+}
+
+loadAnime();
