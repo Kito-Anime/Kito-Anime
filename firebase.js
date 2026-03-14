2b5897dd6fd0f099f504a2af678cba1c7fdade5d
Kito-Anime <antayteam@gmail.com>
2026-03-14 22:07:08 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-"apiKey": "API_KEY",
-"authDomain": "AUTH_DOMAIN",
-"projectId": "PROJECT_ID",
-"storageBucket": "STORAGE_BUCKET",
-"messagingSenderId": "MESSAGING_SENDER_ID",
-"appId": "APP_ID"
-"measurementId": "measurement_Id",
+  apiKey: "AIzaSyBP9ZnwzQrrRY5JNy1DANm3boaWgvhoS_w",
+  authDomain: "kito-anime-vn.firebaseapp.com",
+  projectId: "kito-anime-vn",
+  storageBucket: "kito-anime-vn.firebasestorage.app",
+  messagingSenderId: "287480619398",
+  appId: "1:287480619398:web:f40d137345407f27874c07",
+  measurementId: "G-N29YRE1NQS"
 };
 
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
